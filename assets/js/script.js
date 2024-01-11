7b4f42c96b18d87aefb4a1ab9ff507937688e1dd
SABA SALEH <m.sweet90@gmail.com>
2024-01-11 13:36:26 +0000

    added home.html

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function redirectToHomePage() {
+    
+    window.location.href = 'home.html';
+}
+
+
+function redirectToGuessGame(category) {
+    window.location.href = 'guess-game.html?category=' + category;
+}
